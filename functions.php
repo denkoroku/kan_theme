e43bd88bef396cc863ffbb9a41469b12f5ff4dd9
Cath Riddoch <cathriddoch@gmail.com>
2020-03-05 09:55:47 +0000

    enqueues the javascript

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 
 function kannon_files(){
+    wp_enqueue_script('kannon-js', get_theme_file_uri('/js/scripts-bundled.js', NULL, '1.0', true));
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('bootstrap','//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css');
     wp_enqueue_style('kannon_main_styles', get_stylesheet_uri());
-
 }
 add_action('wp_enqueue_scripts','kannon_files');
 
